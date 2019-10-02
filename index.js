27bb4d4711bb6824456b3bee47eaa7034a21ee5b
Eugen Kondratiev <eugenkondratiev@gmail.com>
2019-10-02 13:00:44 +0300

    initial. get players and update bd

---
+++ index.js
@@ -0,0 +1,9 @@
+require('./scheduler')();
+
+const dbPool = require('./model/connection-pool');
+process.on('beforeExit', (e) => {
+    dbPool.end();
+})
+process.on('exit', (e) => {
+    dbPool.end();
+})
