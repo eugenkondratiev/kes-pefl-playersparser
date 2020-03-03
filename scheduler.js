8e4826fddaebf32dc60bf3df2774c8809a2dc8e8
Eugen Kondratiev <eugenkondratiev@gmail.com>
2020-03-03 14:01:34 +0200

    reschedule to 1update per matchday plus sunday

--- scheduler.js
+++ scheduler.js
@@ -1,14 +1,16 @@
 const schedule = require('node-schedule');
 const getPlayers = require('./model/get-players-base');
-
+const dbQuery = require('./model/db').dbQuery; 
 
 const ruleEveryID = {
-  hour: [3, 11],
-  minute: [14],
+  hour: 3,
+//hour: [3, 10],
+  minute: 14,
   second: 20,
   dayOfWeek: [0, 2, 4, 6]
 };
 // const ruleEveryID = {hour: 11, minute: [36  , 38], second: 14, dayOfWeek: [1, 2, 4, 6]};
+const ruleEveryHour = {minute: 1, second: 4};
 
 module.exports = function () {
   async function playerBaseUpdateAction() {
@@ -28,7 +30,17 @@ module.exports = function () {
   }
 
   playerBaseUpdateAction().then(() => true, (err) => console.log(err));
+  const schDbPing = schedule.scheduleJob(ruleEveryHour, async function(){
+     try {
+      await dbQuery("SELECT 1");
 
+     } catch (error) {
+       console.log(error);
+       const logRecord = new Date() + "  DbPing - error " + error.message + "\n";
+            require('fs').appendFile("./data/actionlog.txt", logRecord, err=>{if (err) console.error(err)});
+     } 
+
+    });
   const schGetPlayers = schedule.scheduleJob(ruleEveryID, function () {
     playerBaseUpdateAction().then(() => true, (err) => console.log(err));
 
