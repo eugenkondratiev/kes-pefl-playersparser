4e3f6c66bcb5e8cfc233bca8e97a31b386d7e476
Eugen Kondratiev <kondratiev.es@gmail.com>
2020-10-01 14:26:52 +0300

    pulled from new ftp

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-require('./scheduler')();
-
-const dbPool = require('./model/connection-pool')();
-process.on('beforeExit', (e) => {
-    dbPool.end();
-})
-process.on('exit', (e) => {
-    dbPool.end();
-=======
 require('./scheduler')();
 
 const dbPool = require('./model/connection-pool-eco')();
@@ -17,5 +7,4 @@ process.on('beforeExit', (e) => {
 })
 process.on('exit', (e) => {
     dbPool.end();
->>>>>>> 6acba38c2c13ee3b989cc08e396e0310d26a915a
 })
