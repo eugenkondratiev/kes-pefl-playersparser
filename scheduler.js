afd0242994cea162df6b8002c63a143ed31c3df5
Eugen Kondratiev <kondratiev.es@gmail.com>
2020-10-24 02:10:57 +0300

    refactor scheduler

--- scheduler.js
+++ scheduler.js
@@ -1,6 +1,9 @@
 const schedule = require('node-schedule');
 const getPlayers = require('./model/get-players-base');
+const updateCups = require('./model/update-cups-base');
 const dbQuery = require('./model/db').dbQuery;
+const fs = require('fs');
+const logIt = require('./logger');
 
 const ruleEveryID = {
   hour: 3,
@@ -15,76 +18,45 @@ const ruleEveryWeek = {
   dayOfWeek: 0
 };
 
-// const ruleEveryID = {hour: 11, minute: [36  , 38], second: 14, dayOfWeek: [1, 2, 4, 6]};
 const ruleEveryHour = { minute: 1, second: 4 };
-const ruleEveryHour2 = { minute: 58, second: 8 };
+// const ruleEveryHour2 = { minute: 58, second: 8 };
 
 module.exports = function () {
 
-  async function cupsUpdateAction() {
-    console.log(new Date(), 'Update cups started');
+  async function updateAction(logFile, msg, cb) {
+    console.log(`${(new Date()).toLocaleString()} ${msg} started`);
     try {
-
-      // const pl = await getPlayers();
       await require('./model/update-cups-base')();
-      const logRecord = new Date() + 'Update cups done\n';
-
-      require('fs').appendFile("cups-actionlog.txt", logRecord, err => {
-        if (err) console.error(err)
-      });
+      logIt(msg)
     } catch (error) {
-      const logRecord = new Date() + error.message + '  \n';
-      require('fs').appendFile("cups-actionlog.txt", logRecord, err => {
-        if (err) console.error(err)
-      });
+      logIt(error.message)
     };
   }
 
-  async function playerBaseUpdateAction() {
-    console.log(new Date(), 'Get players started');
-    try {
 
-      const pl = await getPlayers();
-      const logRecord = new Date() + 'Get players done\n';
-      // await require('./model/update-cups-base')();
-
-      require('fs').appendFile("actionlog.txt", logRecord, err => {
-        if (err) console.error(err)
-      });
-    } catch (error) {
-      const logRecord = new Date() + error.message + '  \n';
-      require('fs').appendFile("actionlog.txt", logRecord, err => {
-        if (err) console.error(err)
-      });
-    };
-  }
-  playerBaseUpdateAction().then(() => true, (err) => console.log("###### - playerBaseUpdateAction",err));
+  updateAction("actionlog.txt", "Get players", getPlayers).then(() => true, (err) => console.log("###### - playerBaseUpdateAction", err));
   setTimeout(() => {
-    cupsUpdateAction().then(() => true, err => console.log("###### - cupsUpdateAction", err));
+    updateAction("cups-actionlog.txt", "Update cups", updateCups).then(() => true, err => console.log("###### - cupsUpdateAction", err));
 
   }, 120000)
-
-  // require('./model/update-cups-base')().then(() => true, (err) => console.log(err));
 
   const schDbPing = schedule.scheduleJob(ruleEveryHour, async function () {
     try {
       await dbQuery("SELECT 1");
     } catch (error) {
       console.log(error);
-      const logRecord = new Date() + "  DbPing - error " + error.message + "\n";
-      require('fs').appendFile("./data/actionlog.txt", logRecord, err => { if (err) console.error(err) });
+      logIt("  DbPing - error " + error.message + "\n")
     }
 
   });
   // const schGetPlayers = schedule.scheduleJob(ruleEveryHour, function () {
   const schGetPlayers = schedule.scheduleJob(ruleEveryID, function () {
-    playerBaseUpdateAction().then(() => true, (err) => console.log(err));
+    updateAction("actionlog.txt", "Get players", getPlayers).then(() => true, (err) => console.log(err));
 
-    // j.cancel();
   });
   // const schParseCups = schedule.scheduleJob(ruleEveryHour2, function () {
   const schParseCups = schedule.scheduleJob(ruleEveryWeek, async function () {
-    cupsUpdateAction().then(() => true, (err) => console.log(err));
+    updateAction("cups-actionlog.txt", "Update cups", updateCups).then(() => true, (err) => console.log(err));
     ;
   });
 
